5bf1b3ec49fa500539b866a4f1df48363a107ece
cyberbeann <100129704+cyberbeann@users.noreply.github.com>
2022-03-13 16:10:08 +0800

    Update script.js

--- location-based-ar/script.js
+++ location-based-ar/script.js
@@ -51,7 +51,7 @@ function renderPlaces(places) {
 
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '0.5 0.5 0.5');
