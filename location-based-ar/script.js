899f929ca2702c20c29f4a09200feb4d77fbab8f
cyberbeann <100129704+cyberbeann@users.noreply.github.com>
2022-03-13 20:02:58 +0800

    Update script.js

--- location-based-ar/script.js
+++ location-based-ar/script.js
@@ -79,7 +79,7 @@ function renderPlaces(places) {
        model.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
        //model.setAttribute('rotation', '0 180 0');
        //model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+       model.setAttribute('scale', '1.0 1.0 1.0');
        model.setAttribute('position', '-3 0.59 -3');
        //model.setAttribute('position', 'absolute');
 
