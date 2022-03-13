b67c25bf6050bbfd9d186df44e3190f425872900
cyberbeann <100129704+cyberbeann@users.noreply.github.com>
2022-03-13 19:30:37 +0800

    Update script.js

--- location-based-ar/script.js
+++ location-based-ar/script.js
@@ -8,32 +8,39 @@ function staticLoadPlaces() {
        {
            name: 'chevrons',
            location: {
-               lat: 1.336685, 
-               lng: 103.702723,
+               lat: 1.3365811, 
+               lng: 103.7022626,
+           }
+       },
+       {
+           name: 'chevrons',
+           location: {
+               lat: 1.336784, 
+               lng: 103.703321,
            }
        },
 
        {
         name: 'chevrons',
         location: {
-            lat: 1.336747, 
-            lng: 103.703091,
+            lat: 1.336725, 
+            lng: 103.703227,
         }
     } ,
 
     {
         name: 'chevrons',
         location: {
-            lat: 1.3367225,
-            lng: 103.7023373,
+            lat: 1.336734,
+            lng: 103.702891,
         }
     },
 
     {
         name: 'chevrons',
         location: {
-            lat: 1.336854, 
-            lng: 103.703408,
+            lat: 1.336743, 
+            lng: 103.702785,
         }
     },
       
@@ -41,16 +48,16 @@ function staticLoadPlaces() {
   {
         name: 'chevrons',
         location: {
-            lat: 1.336905, 
-            lng: 103.703711,
+            lat: 1.336690, 
+            lng: 103.702520,
         }
     },
        
         {
         name: 'chevrons',
         location: {
-            lat: 1.336255,
-            lng: 103.702904,
+            lat: 1.3370507,
+            lng: 103.7037153,
         }
     }
        
@@ -70,7 +77,7 @@ function renderPlaces(places) {
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
+       //model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '0.5 0.5 0.5');
 
