16503a96ced391954e90593d96fd5089274f0960
cyberbeann <100129704+cyberbeann@users.noreply.github.com>
2022-04-09 19:35:57 +0800

    Add files via upload

---
+++ working/arjs.js
@@ -0,0 +1,52 @@
+window.parent.document.getElementById("goCamera").addEventListener("click", () => loadRoute());
+
+function loadRoute() {
+    var arr = window.parent.window.places;
+    renderPlaces(arr);
+    console.log("working");
+}
+
+
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+     
+     let end = document.createElement('a-entity');
+     let end_lat = places[0].location.lat;
+     let end_lng = places[0].location.lng;
+
+     end.setAttribute('id', '-1');
+     end.setAttribute('gps-entity-place', `latitude: ${end_lat}; longitude: ${end_lng};`);
+     end.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
+     end.setAttribute('scale', '2 2 2');
+     end.setAttribute('position', '1 2 3');
+     end.setAttribute('position', 'absolute');
+     
+     var pos = end.object3D.position;
+     scene.appendChild(end);
+    
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+        let id = place.name;
+ 
+        let model = document.createElement('a-entity');
+        
+        model.setAttribute('id', id.toString());
+        model.setAttribute('look-at', pos);
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
+        model.setAttribute('scale', '2 2 2');
+        model.setAttribute('position', '1 2 3');
+        model.setAttribute('position', 'absolute');
+ 
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+        
+        pos = model.object3D.position;
+ 
+        scene.appendChild(model);
+        console.log("seems to be working");
+    });
+}
