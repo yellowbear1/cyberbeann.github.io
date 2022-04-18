83c30a28a3dcd9ebaeaec476f3f51fb3fbfdc8e4
cyberbeann <100129704+cyberbeann@users.noreply.github.com>
2022-04-18 17:14:51 +0800

    Update arjs.js

--- working/arjs.js
+++ working/arjs.js
@@ -17,7 +17,7 @@ function renderPlaces(places) {
 
      end.setAttribute('id', '-1');
      end.setAttribute('gps-entity-place', `latitude: ${end_lat}; longitude: ${end_lng};`);
-     end.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
+     end.setAttribute('gltf-model', './assets/map_pointer_3d_icon/scene.gltf');
      end.setAttribute('scale', '2 2 2');
      end.setAttribute('position', '1 2 3');
      end.setAttribute('position', 'absolute');
@@ -35,7 +35,7 @@ function renderPlaces(places) {
         model.setAttribute('id', id.toString());
         model.setAttribute('look-at', pos);
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/chevrons/scene.gltf');
+        model.setAttribute('gltf-model', './assets/red_arrow_chevrons_wayfinding/scene.gltf');
         model.setAttribute('scale', '2 2 2');
         model.setAttribute('position', '1 2 3');
         model.setAttribute('position', 'absolute');
