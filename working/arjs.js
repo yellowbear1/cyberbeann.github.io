41731f39350c4ba61bd8d7f8a1d6e871deccbf20
cyberbeann <100129704+cyberbeann@users.noreply.github.com>
2022-05-11 17:19:18 +0800

    Update arjs.js

--- working/arjs.js
+++ working/arjs.js
@@ -47,15 +47,17 @@ function renderPlaces(places) {
      let end = document.createElement('a-entity');
      let end_lat = places[0].location.lat;
      let end_lng = places[0].location.lng;
+     let end_id = places[0].name;
+     
 
-     end.setAttribute('id', '-1');
+     end.setAttribute('id', "node" + end_id);
      end.setAttribute('gps-entity-place', `latitude: ${end_lat}; longitude: ${end_lng};`);
      end.setAttribute('gltf-model', './assets/map_pointer_3d_icon/scene.gltf');
      end.setAttribute('scale', '0.5 0.5 0.5');
      end.setAttribute('position', '1 2 3');
      end.setAttribute('position', 'absolute');
-     
-     var pos = end.object3D.position;
+
+     console.log("^end" + "node" + end_id)
      scene.appendChild(end);
     
 
@@ -71,23 +73,26 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
         let id = place.name;
+        
  
         let model = document.createElement('a-entity');
         
-        model.setAttribute('id', id.toString());
-        model.setAttribute('look-at', pos);
+        model.setAttribute('id', "node" + id.toString());
+        model.setAttribute('look-at', '#node' + (id+1).toString());
+        console.log("For " + id.toString());
+        console.log("node" + (id+1).toString());
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', './assets/red_arrow_chevrons_wayfinding/scene.gltf');
         model.setAttribute('scale', '0.02 0.02 0.02');
         model.setAttribute('position', '1 2 3');
         model.setAttribute('position', 'absolute');
-        model.setAttribute('rotation', '90 -45 0');
+        model.setAttribute('rotation', '90 -90 0');
  
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
         
-        pos = model.object3D.position;
+        
  
         scene.appendChild(model);
         
